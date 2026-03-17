5f69fe26e2954f018cb4e1a23faf6b7036f35ed3
Keerti <keertirao111@gmail.com>
2026-03-17 20:06:55 +0530

    Initial commit - MCA II Data Structure and Algorithm practicals

---
+++ practical_2.py
@@ -0,0 +1,8 @@
+# Search element in a list
+lst = [5, 15, 25, 35, 45]
+print("List:", lst)
+key = int(input("Enter element to search: "))
+if key in lst:
+ print("Element found at index:", lst.index(key))
+else:
+ print("Element not found")
